9a347e7743b8f52393cedabec94206adb7047587
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-19 16:04:55 +0100

    refined show page, added edit and update pages

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,26 +1,38 @@
 class FlatsController < ApplicationController
-  before_action :set_flat, only: %i[new create show edit update destroy]
+  before_action :set_flat, only: %i[show edit update destroy]
 
   def index
     @flats = Flat.all
+    @flats_number = Flat.count
   end
 
   def show
   end
 
   def new
+    @flat = Flat.new
   end
 
   def create
+    @flat = Flat.new(flat_params)
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def edit
   end
 
   def update
+    @flat.update(flat_params)
+    redirect_to flat_path(@flat)
   end
 
   def destroy
+    @flat.destroy
+    redirect_to flats_path, status: :see_other
   end
 
   private
@@ -30,7 +42,7 @@ class FlatsController < ApplicationController
   end
 
   def set_flat
-    @flat = Flat.find(params[:flat_id])
+    @flat = Flat.find(params[:id])
   end
 end
 
