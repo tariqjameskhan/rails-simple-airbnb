e951d80c702b6a1468e72b8734fc34ae12e91663
tariq khan <tariqjameskhan@hotmail.co.uk>
2023-05-19 16:06:23 +0100

    tidy up

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -45,32 +45,3 @@ class FlatsController < ApplicationController
     @flat = Flat.find(params[:id])
   end
 end
-
-
-
-# def index
-#   @restaurants = Restaurant.all
-# end
-
-# def show
-#   @restaurant = Restaurant.find(params[:id])
-# end
-
-# def new
-#   @restaurant = Restaurant.new
-# end
-
-# def create
-#   @restaurant = Restaurant.new(restaurant_params)
-#   if @restaurant.save
-#     redirect_to restaurant_path(@restaurant)
-#   else
-#     render :new, status: :unprocessable_entity
-#   end
-# end
-
-# private
-
-# def restaurant_params
-#   params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-# end
